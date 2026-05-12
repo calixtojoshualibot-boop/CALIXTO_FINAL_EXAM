0acefa7b1e7ede8b3fb1d30f34eebca96f28406d
calixtojoshualibot-boop <calixtojoshualibot@gmail.com>
2026-05-12 12:22:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the current directory
-app.use(express.static(__dirname));
+// This ensures the server looks in the correct directory regardless of where it's launched from
+const publicPath = path.join(__dirname, ''); 
 
-// Send index.html when someone visits the site
+app.use(express.static(publicPath));
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(publicPath, 'index.html'), (err) => {
+        if (err) {
+            res.status(404).send("File not found! Make sure index.html is in the root folder.");
+        }
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is live at port ${PORT}`);
 });
